ce2374156e6109e69cf6983e65970a1b13e55309
rinrinmame <rina143606@gmail.com>
2024-04-23 02:25:21 +0900

    4/22

--- js/main.js
+++ js/main.js
@@ -13,7 +13,19 @@ jQuery( function( $ ) {
 jQuery( function( $ ) {
 	if (window.matchMedia( '(max-width: 1203px)' ).matches) {
 		$( ".js-menu__button" ).on( "click", function() {
+			$( ".l-body" ).toggleClass( "is-open" );
 			$( ".l-sidebar" ).toggleClass( "is-open" );
+			$( ".c-button__hamburger" ).toggleClass( "is-open" );
+		} );
+	}
+} );
+
+jQuery( function( $ ) {
+	if (window.matchMedia( '(max-width: 1203px)' ).matches) {
+		$( ".js-hamburger__button" ).on( "click", function() {
+			$( this ).removeClass( "is-open" );
+			$( ".l-sidebar" ).removeClass( "is-open" );
+			$( ".l-body" ).removeClass( "is-open" );
 		} );
 	}
 } );
